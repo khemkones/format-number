bb58f75e99085fcd03ff0e3b317782cda89ab5ee
Khemkonez <khemkones@gmail.com>
2023-07-14 17:42:39 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-exports.formatstringNumber = (stringNumber, format) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const digits = stringNumber.replace(/\s/g, '');
-            let formattedNumber = '';
-            let digitIndex = 0;
+const formatStringNumber = function (stringNumber, format) {
+    const digits = stringNumber.replace(/\s/g, '');
+    let formattedNumber = '';
+    let digitIndex = 0;
 
-            // Iterate over the format and replace placeholders with digits
-            for (let i = 0; i < format.length; i++) {
-                if (format[i] === 'X') {
-                    // Replace 'X' with the corresponding digit
-                    formattedNumber += digits[digitIndex] || '';
-                    digitIndex++;
-                } else {
-                    // Copy non-placeholder characters as-is
-                    formattedNumber += format[i];
-                }
-            }
-
-            resolve(formattedNumber);
-        } catch (e) {
-            reject(e);
+    // Iterate over the format and replace placeholders with digits
+    for (let i = 0; i < format.length; i++) {
+        if (format[i] === 'X') {
+            // Replace 'X' with the corresponding digit
+            formattedNumber += digits[digitIndex] || '';
+            digitIndex++;
+        } else {
+            // Copy non-placeholder characters as-is
+            formattedNumber += format[i];
         }
-    });
-}
+    }
+
+    return formattedNumber
+};
+
+module.exports = formatStringNumber;
 
